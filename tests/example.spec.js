a78167aa57cfc21898b68f1995e2356ddf0c3a4b
Lidia Barkanova <Lidia.Barkanova@infobip.com>
2025-02-23 10:20:27 +0100

    separate branch for github pages

--- tests/example.spec.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('has title', async ({ page }) => {
-  await page.goto('https://teamlida.github.io/tad-2025/');
-  await expect(page).toHaveTitle(/Coffee Ordering System/);
-});
